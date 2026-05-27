77e1344efac5964159127f98a3eef116d2be1203
Vasundhara Ravi <vasundhararavi641@gmail.com>
2026-05-27 10:40:55 +0200

    Added remaining files to git

---
+++ src/main/java/com/security/incidentmanager/dto/mapper/TagMapper.java
@@ -0,0 +1,26 @@
+package com.security.incidentmanager.dto.mapper;
+
+import com.security.incidentmanager.domain.Tag;
+import com.security.incidentmanager.dto.request.TagRequestDTO;
+import com.security.incidentmanager.dto.response.TagResponseDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TagMapper {
+
+    public TagResponseDTO toResponseDTO(Tag tag) {
+        TagResponseDTO dto = new TagResponseDTO();
+        dto.setId(tag.getId());
+        dto.setName(tag.getName());
+        dto.setColor(tag.getColor());
+        dto.setCreatedAt(tag.getCreatedAt());
+        return dto;
+    }
+
+    public Tag toEntity(TagRequestDTO dto) {
+        Tag tag = new Tag();
+        tag.setName(dto.getName());
+        tag.setColor(dto.getColor());
+        return tag;
+    }
+}
---
+++ src/main/java/com/security/incidentmanager/dto/request/TagRequestDTO.java
@@ -0,0 +1,9 @@
+package com.security.incidentmanager.dto.request;
+
+import lombok.Data;
+
+@Data
+public class TagRequestDTO {
+    private String name;
+    private String color;
+}
